d4204de1b1d6f19ccce7e9828a77799d7713408e
cruisers <89080385@qq.com>
2017-05-23 11:25:44 +0900

    Python

---
+++ Python/70518.py
@@ -0,0 +1,14 @@
+student={"Zhang":113,"Li":114}
+print(student["Zhang"])                         #Dictionary
+#--------------------------------------------
+color={
+	"R":[255,0,0],
+	"G":[0,255,0],
+	"B":[0,0,255]
+}
+print(color["G"])
+#--------------------------------------------
+lalala={1:5,2:10,3:"wow",4:3.14}
+lalala[3]=23232
+lalala[1]="www"                                   #这些dictionary可以自由变换
+print(lalala)
